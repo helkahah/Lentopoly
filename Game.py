77dbc15bf9bf1976dc09a4eadee6b7d4aefa8682
SolHonka <salomonhonk@gmail.com>
2023-02-27 13:48:34 +0200

    Prototype 0.3.0

--- Game.py
+++ Game.py
@@ -9,8 +9,46 @@ PlayerData = {
     "Airports": {}
 }
 
+Cycle = 0
 
 #print(GetAirportPrice("Total Rf Heliport"))
 
 print(Airports.Get("Total Rf Heliport"))
 print(Airports.Get("Total Rf Heliport"))
+
+#Menu Functions
+
+def PromptActions(Actions):
+    ActionCount = 1
+
+    CurrentActions = {}
+
+    for Key in Actions:
+        print(ActionCount, Key)
+        CurrentActions[ActionCount] = Key
+        ActionCount += 1
+
+    Action = int(input())
+
+    CurrentActions[Action]
+
+    return CurrentActions[Action]
+def Menu_Buy():
+    print("Test")
+
+
+Menus = {
+    "Buy": Menu_Buy,
+    "Sell": None,
+    "Loan": None,
+}
+
+while Cycle <= 20:
+    Cycle += 1
+    Action = PromptActions(
+        [
+            "Buy",
+            "Sell",
+            "Continue",
+        ]
+    )
