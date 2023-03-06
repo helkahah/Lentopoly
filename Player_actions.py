bab6b58784c01b805989a0b5951c6e512950558b
helkahah <helka.hahkio@metropolia.fi>
2023-03-06 14:07:58 +0200

    Player actions

---
+++ Player_actions.py
@@ -0,0 +1,16 @@
+import PlayerData
+import Airports
+def upkeep():
+    for i in PlayerData.Airports():
+        input(f'Haluatko maksaa lentokentän '{PlayerData.Airports}' ylläpidon? Maksettavaa on: '{Airports.})
+
+
+def BuyAirport(Name):
+
+
+def SellAirport():
+
+def TakeLoan():
+
+def PayLoan():
+
